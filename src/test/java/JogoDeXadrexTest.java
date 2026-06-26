5ae3e4cabe2691e1926843cfbe61ee8a3daf9ec7
Biancanoronha <biancateixeirawtn@gmail.com>
2026-06-25 22:32:54 -0300

    ola

--- src/test/java/JogoDeXadrexTest.java
+++ src/test/java/JogoDeXadrexTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 public class JogoDeXadrexTest {
     @Test
     void testeCriarObjetos() {
-        Peca peca = new Peca("Qualquer coisa");
-        assertEquals("Qualquer coisa",peca.getNome());
+        Torre torre = new Torre("Qualquer coisa");
+        assertEquals("Qualquer coisa",torre.getNome());
     }
 }
